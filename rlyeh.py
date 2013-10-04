424f2d2d0a78f1b58a832faa92d09c1a43a70a0c
segfault <inhumanundead@gmail.com>
2013-10-03 22:08:04 -0500

    Added some attacking shit

--- rlyeh.py
+++ rlyeh.py
@@ -25,23 +25,24 @@ eld1 = sf.Sprite(sf.Texture.from_file("eldritch1.png"))
 # The player character
 player = sf.Sprite(sf.Texture.from_file("player.png"))
 # Player HP, if you get hit 10 times you die.
-hp = 300
+hp = 50
 # Whetier you're attacking or not.
 attacking = False
 # A clock that makes sure you only get to attack for 20 seconds straight before taking a "breath" per se
 attacking_clock = sf.Clock()
 
-move_dict = {sf.Keyboard.W:sf.Vector2(0, -5), sf.Keyboard.A:sf.Vector2(-5, 0), sf.Keyboard.S:sf.Vector2(0, 5), sf.Keyboard.D:sf.Vector2(5, 0)}
+move_dict = {sf.Keyboard.W:sf.Vector2(0, -7), sf.Keyboard.A:sf.Vector2(-7, 0), sf.Keyboard.S:sf.Vector2(0, 7), sf.Keyboard.D:sf.Vector2(7, 0)}
 refresh_clock = sf.Clock()
 spawn_clock = sf.Clock()
 cth_dead = False
 enemies_list = []
+spawning = True
 
 while w.is_open:
     for e in w.events:
         if type(e) is sf.KeyEvent and e.pressed:
             if e.code is sf.Keyboard.SPACE:
-                if attacking and attacking_clock.elapsed_time.seconds >= 10:
+                if attacking and attacking_clock.elapsed_time.seconds >= 5:
                     attacking = False
                 elif not attacking:
                     attacking = True
@@ -49,10 +50,16 @@ while w.is_open:
             elif e.code is sf.Keyboard.ESCAPE:
                 w.close()
             else:
-                player.move(move_dict[e.code])
+                try:
+                    player.move(move_dict[e.code])
+                except KeyError:
+                    pass
 
-    if spawn_clock.elapsed_time.seconds >= .10 and not cth_dead:
-        if len(enemies_list) > 10:
+    if attacking_clock.elapsed_time.seconds >= 5 and attacking:
+        attacking = False
+
+    if spawn_clock.elapsed_time.seconds >= .10 and not cth_dead and spawning:
+        if len(enemies_list) > 20:
             del enemies_list[0:5]
         spawn_clock.restart()
         y = sf.Sprite(sf.Texture.from_file("eldritch1.png"))
@@ -70,11 +77,15 @@ while w.is_open:
 
     if collision.collides(cth.global_bounds, player.global_bounds) and attacking:
         cth_hp -= 1
+        attacking = False
+        cth.position = sf.Vector2(random.randint(0, 596), random.randint(0, 480-64))
 
     for i in enemies_list:
-        if collision.collides(i.global_bounds, player.global_bounds) and refresh_clock.elapsed_time.seconds > 1.5:
-            hp -= 10
+        if collision.collides(i.global_bounds, player.global_bounds) and refresh_clock.elapsed_time.seconds > 1 and not attacking:
+            hp -= 5
             refresh_clock.restart()
+        elif collision.collides(i.global_bounds, player.global_bounds) and attacking:
+            enemies_list.remove(i)
         else:
             continue
 
@@ -96,4 +107,6 @@ while w.is_open:
         enemies_list[i].move(sf.Vector2(random.randint(-10, 10), random.randint(-10, 10)))
         w.draw(enemies_list[i])
     print hp
+    print len(enemies_list)
+    print attacking
     w.display()
