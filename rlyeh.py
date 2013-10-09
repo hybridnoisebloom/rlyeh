ee789c2f247871051f52e55408126c90feab261a
segfault <inhumanundead@gmail.com>
2013-10-09 17:30:24 -0500

    FINAL BUILD!

--- rlyeh.py
+++ rlyeh.py
@@ -2,11 +2,9 @@ import sfml as sf
 import collision
 import random
 import time
+import json
 
 ### TO-DO LIST! ###
-## TODO: Add logo screen ##
-## TODO: Add JSON save games ##
-## TODO: Multiple levels (randomize which Great Old One is in which level) ##
 
 # collision.collides is the collision detection function, it returns a boolean.
 
@@ -33,6 +31,9 @@ title2 = sf.Texture.from_file("title2.png") # Play button
 title3 = sf.Texture.from_file("title3.png") # Credits button
 title4 = sf.Texture.from_file("title4.png") # Exit button
 
+winnar_winnar_chicken_dinner = sf.Sprite(sf.Texture.from_file("sortawinrar.png"))
+you_are_a_fucking_failure = sf.Sprite(sf.Texture.from_file("awwman.png"))
+
 t1 = sf.Sprite(title1)
 t1.move(sf.Vector2(320-128, 10))
 t2 = sf.Sprite(title2)
@@ -52,9 +53,7 @@ aza = sf.Sprite(sf.Texture.from_file("azathoth.png"))
 yog = sf.Sprite(sf.Texture.from_file("yog-sothoth.png"))
 shu = sf.Sprite(sf.Texture.from_file("shub-niggurath.png"))
 
-cth.move(sf.Vector2(random.randint(200, 500), random.randint(200, 450)))
-
-cth_hp = 50
+goo_hp = random.randint(10, 500)
 # The generic-brand eldritch monsters you fight in the game
 eld1 = sf.Sprite(sf.Texture.from_file("eldritch1.png"))
 #eld2 = sf.Sprite(sf.Texture.from_file("eldritch2.png"))
@@ -73,7 +72,7 @@ attacking_clock = sf.Clock()
 move_dict = {sf.Keyboard.W:sf.Vector2(0, -7), sf.Keyboard.A:sf.Vector2(-7, 0), sf.Keyboard.S:sf.Vector2(0, 7), sf.Keyboard.D:sf.Vector2(7, 0)}
 refresh_clock = sf.Clock()
 spawn_clock = sf.Clock()
-cth_dead = False
+goo_dead = False
 enemies_list = []
 spawning = True
 level = -1
@@ -81,6 +80,16 @@ level = -1
 # 0 = title
 # 1 = actual game
 logo_clock = sf.Clock()
+
+winrar = False
+
+goo_num = 0 # great old one number, randomised.
+
+goo = [cth, yog, nya, shu, aza]
+goo[goo_num].move(sf.Vector2(random.randint(0, 640), random.randint(0, 480)))
+
+difficulty = 1
+skill_lvls = 0
 
 while w.is_open:
     if level == 1 and not music_playing:
@@ -96,6 +105,12 @@ while w.is_open:
                     attacking_clock.restart()
             elif e.code is sf.Keyboard.ESCAPE:
                 w.close()
+            elif e.code is sf.Keyboard.L_BRACKET:
+                goo_hp = 0
+            elif e.code is sf.Keyboard.R_BRACKET:
+                skill_lvls = 999999
+                difficulty = 1000000
+                goo_hp = 1
             else:
                 try:
                     player.move(move_dict[e.code])
@@ -107,6 +122,12 @@ while w.is_open:
                     if title_buffer[i].texture == title2:
                         startgame.play()
                         level = 1
+                        try:
+                            difficulty = json.load(open("save.json"))["difficulty"]
+                            skill_lvls = json.load(open("save.json"))["skill_lvl"]
+                        except IOError:
+                            difficulty = 1
+                            skill_lvls = 0
                     elif title_buffer[i].texture == title3:
                         thingy.restart()
                         level = 3
@@ -117,14 +138,22 @@ while w.is_open:
         elif type(e) is sf.KeyEvent and e.pressed and e.code == sf.Keyboard.ESCAPE and level == 3:
             level = 0
 
+    if level == 1 and skill_lvls == 1000000:
+        winner_clock = sf.Clock()
+        hp = pow(100, 100)
+        level = 4
+        winrar = True
+        if winner_clock.elapsed_time.seconds >= 3:
+            w.close()
+
     if logo_clock.elapsed_time.seconds >= 5 and level == -1:
         level = 0
 
     if attacking_clock.elapsed_time.seconds >= 5 and attacking:
         attacking = False
 
-    if spawn_clock.elapsed_time.seconds >= .10 and not cth_dead and spawning:
-        if len(enemies_list) > 20:
+    if spawn_clock.elapsed_time.seconds >= .10 and not goo_dead and spawning:
+        if len(enemies_list) > 20*difficulty:
             del enemies_list[0:5]
         spawn_clock.restart()
         y = sf.Sprite(sf.Texture.from_file("eldritch1.png"))
@@ -143,13 +172,13 @@ while w.is_open:
     if not spawning and spawn_clock.elapsed_time.seconds >= 5:
         spawning = True
 
-    if collision.collides(cth.global_bounds, player.global_bounds) and attacking:
+    if collision.collides(goo[goo_num].global_bounds, player.global_bounds) and attacking:
         cthulhuhit.play()
-        cth_hp -= 10
+        goo_hp -= 10
         attacking = False
         spawning = False
         spawn_clock.restart()
-        cth.position = sf.Vector2(random.randint(0, 596), random.randint(0, 480-64))
+        goo[goo_num].position = sf.Vector2(random.randint(0, 596), random.randint(0, 480-64))
 
     for i in enemies_list:
         if collision.collides(i.global_bounds, player.global_bounds) and refresh_clock.elapsed_time.seconds > 1 and not attacking:
@@ -164,30 +193,37 @@ while w.is_open:
         if i.position.x < 0 or i.position.y < 0:
             enemies_list.remove(i)
 
-    if cth.position.x < 0 or cth.position.y < 0 or cth.position.x > 640 or cth.position.y > 480:
-        cth.position = sf.Vector2(random.randint(0, 640-64), random.randint(0, 480-64))
+    if goo[goo_num].position.x < 0 or goo[goo_num].position.y < 0 or goo[goo_num].position.x > 640 or goo[goo_num].position.y > 480:
+        goo[goo_num].position = sf.Vector2(random.randint(0, 640-64), random.randint(0, 480-64))
 
-    if player.position.x < 0 or cth.position.y < 0 or player.position.x > 640 or player.position.y > 480:
+    if player.position.x < 0 or goo[goo_num].position.y < 0 or player.position.x > 640 or player.position.y > 480:
         player.position = sf.Vector2(random.randint(0, 640), random.randint(0, 480))
 
-    if cth_hp <= 0:
-        cth_dead = True
+    if goo_hp <= 0:
+        goo_dead = True
 
     if hp <= 0:
-        w.close()
-        print "You lose!"
+        level = 4
+        winrar = False
+        loser_clock = sf.Clock()
+        if loser_clock.elapsed_time.seconds >= 3:
+            w.close()
+    if goo_dead:
+        goo_num = random.randint(0, 4)
+        goo_dead = False
+        goo_hp = random.randint(10, 500)
+        skill_lvls += 1
+        difficulty += 1
+        json.dump({"skill_lvl": int(skill_lvls), "difficulty": difficulty}, open("save.json", "w"))
 
-    if cth_dead:
-        w.close()
-        print "You win!"
     w.clear(sf.Color.RED)
     if level == -1:
         w.draw(logo)
     elif level == 1:
         w.draw(bg2)
         w.draw(player)
-        cth.move(sf.Vector2(random.randint(-5, 5), random.randint(-5, 5)))
-        w.draw(cth)
+        goo[goo_num].move(sf.Vector2(random.randint(-5, 5), random.randint(-5, 5)))
+        w.draw(goo[goo_num])
         for i in range(0, len(enemies_list)):
             enemies_list[i].move(sf.Vector2(random.randint(-10, 10), random.randint(-10, 10)))
             w.draw(enemies_list[i])
@@ -195,11 +231,16 @@ while w.is_open:
         w.draw(bg1)
         for i in range(0, len(title_buffer)):
             w.draw(title_buffer[i])
-            print title_buffer
     elif level == 3:
         w.draw(bg1)
         w.draw(credits)
-    print hp
-    print len(enemies_list)
-    print attacking
+    elif level == 4:
+        if winrar:
+            w.draw(winnar_winnar_chicken_dinner)
+        elif not winrar:
+            w.draw(you_are_a_fucking_failure)
+    print "Player HP: "+str(hp)
+    print "Boss HP: "+str(goo_hp)
+    print "Level: "+str(skill_lvls)
+    print "Difficulty: "+str(difficulty)
     w.display()
